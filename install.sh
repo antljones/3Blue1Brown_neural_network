e2b15c03b4536c1ac185d1541bb37ee01bcd0396
antljones <=>
2023-04-12 21:00:57 +0000

    Added installer and general folder layout

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+
+sudo yum -y install python3-devel libjpeg libjpeg-devel zlib libtiff freetype freetype-devel libwebp tcl/tk openjpeg libimagequant libraqm
+
+python3 -m pip install Pillow
+python3 -m pip install numpy
