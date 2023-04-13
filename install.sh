a94d5c7d578d5ef9e85685ff64c37b07e73129c4
antljones <=>
2023-04-13 01:34:37 +0000

    Added gitignore

--- install.sh
+++
@@ -1,6 +0,0 @@
-
-
-sudo yum -y install python3-devel libjpeg libjpeg-devel zlib libtiff freetype freetype-devel libwebp tcl/tk openjpeg libimagequant libraqm
-
-python3 -m pip install Pillow
-python3 -m pip install numpy
