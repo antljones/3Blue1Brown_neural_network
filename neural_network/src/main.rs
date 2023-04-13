211a5922fd83d1bb91a570c622271bc68c416023
antljones <=>
2023-04-13 23:10:21 +0000

    Added input layer

--- neural_network/src/main.rs
+++ neural_network/src/main.rs
@@ -1,3 +1,15 @@
+use image::*;
+
 fn main() {
+  const IMAGE_PIXEL_AMOUNT: usize = 784;
+
+  let mut input_layer: Vec<&Luma<u8>> = Vec::with_capacity(IMAGE_PIXEL_AMOUNT);
+
+  let image_to_load = image::open("../../input_image/test_image.bmp").expect("test image not found").to_luma8();
+
+  for pixel in image_to_load.pixels() { 
+    input_layer.push(pixel);
+
+  }
 
 }
